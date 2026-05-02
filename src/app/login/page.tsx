42b9c6c628af104c6b784e8978418015483cd1b9
kartik7454 <kb848067@gmail.com>
2026-05-02 16:24:22 +0530

    finaell

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,13 @@ export default function Login() {
       <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
         <div className='w-full flex flex-col items-center max-w-md space-y-8'>
           <div className='flex flex-col items-center gap-8'>
-            logo
+            <img
+              src="https://i.pinimg.com/736x/31/8e/e9/318ee9b4a5d1d456e395301ac123e1a4.jpg"
+              alt="Pizza Logo"
+              className="w-40 h-40 rounded-full shadow-lg mb-4"
+            />
+      
+      
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
               Sign in to your account
             </h2>
